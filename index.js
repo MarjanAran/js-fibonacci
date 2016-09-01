180f4a05fc1459b68f74a81d4cc3dc3f55fb2f9b
Mariya Ishchuk <mariya.ishchuk@me.com>
2016-09-01 13:35:44 +0000

    fibonacci

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var result, i;
 var sum1 = 0, sum2 = 1; // 0 and 1
-    if(n == 0) return sum1;    //result for 0
-    if(n == 1 || n == -1) return sum2;    //result for 1
-    if(n > 0) {              //for positive numbers
-        for( i = 2; i <= n; i++) {
+    if (n == 0) return sum1;    //result for 0
+    if (n == 1 || n == -1) return sum2;    //result for 1
+    if (n > 0) {              //for positive numbers
+        for ( i = 2; i <= n; i++) {
             result = sum1 + sum2;
             sum1 = sum2;
             sum2 = result;
